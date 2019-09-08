7a187405e0dc44adde5bce20812c2ea4fb7d62e5
Neófito Hermetista <void@localhost.localdomain>
2019-09-08 15:54:24 -0300

    bug fixes (changeState não provoca mais segfault)

---
+++ enonplayable.cpp
@@ -0,0 +1,6 @@
+#include "enonplayable.h"
+
+ENonPlayable::ENonPlayable()
+{
+
+}
---
+++ enonplayable.h
@@ -0,0 +1,11 @@
+#ifndef ENONPLAYABLE_H
+#define ENONPLAYABLE_H
+
+
+class ENonPlayable
+{
+public:
+    ENonPlayable();
+};
+
+#endif // ENONPLAYABLE_H
